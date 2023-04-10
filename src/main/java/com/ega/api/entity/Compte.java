b6f9477b427db079f7eab371e8d3e853941655a8
Achille DJAKPO <shwarzenegerachille@gmail.com>
2023-04-10 11:16:01 +0000

    fixing hibernateLazyInitializer error message

--- src/main/java/com/ega/api/entity/Compte.java
+++ src/main/java/com/ega/api/entity/Compte.java
@@ -1,6 +1,7 @@
 package com.ega.api.entity;
 
-import com.ega.api.entity.Client;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -34,5 +35,6 @@ public class Compte {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Client client;
 }
