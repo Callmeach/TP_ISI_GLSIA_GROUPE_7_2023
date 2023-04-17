f318d3d4ee0190a54c3b3cf0ce371d77cd48ef10
hippolyte93 <ippolytekoessan@gmail.com>
2023-04-17 23:25:48 +0000

    création du serviceClient et modification du ClientController

--- src/main/java/com/ega/api/repository/ClientRepository.java
+++ src/main/java/com/ega/api/repository/ClientRepository.java
@@ -4,7 +4,12 @@ import com.ega.api.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+    Optional<Object> findByEmail(String email);
+
 
 }
